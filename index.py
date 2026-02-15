97b470a7d1a7e840e7353d56bd9ad9c2da0765e1
b-d-alan <balthasardanielo@gmail.com>
2026-02-15 20:07:59 +0530

    setup table votes

--- index.py
+++ index.py
@@ -6,3 +6,8 @@ print(os.getcwd())
 import sqlite3
 
 conn = sqlite3.connect("test.db")
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS votes 
+              ( id INTEGER PRIMARY KEY AUTOINCREMENT,
+               party TEXT NOT NULL,
+               votes INTEGER DEFAULT 0)""" )
