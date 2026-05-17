7c355b67f17e180ca71c34bcd66afb9246e946fd
b-d-alan <balthasardanielo@gmail.com>
2026-05-17 17:22:43 +0530

    clean up comments and improve file header description

--- index.py
+++ index.py
@@ -1,37 +1,6 @@
 """
-a simple program for voting
-this file is going to have just the backend logic
+this file is going to have just the backend logic for voting
 """
-
-import os
-import sqlite3
-
-"""print(os.getcwd())
-
-connection_object_to_testdb = sqlite3.connect("test.db")
-# ":memory:" for an inmemory database
-cursor = connection_object_to_testdb.cursor()
-# cursor.execute(\"""CREATE TABLE test_table(
-#               party text,
-#               candidate text,
-#               votes integer)\""")
-
-# cursor.execute("INSERT INTO test_table VALUES ('party_2','candidate_2',0)")
-
-# connection_object_to_testdb.commit()
-
-# cursor.fetchone()
-
-# cursor.fetchmany(number)
-
-cursor.execute("SELECT * FROM test_table  ")
-print([entry for entry in cursor.fetchall()])
-print(cursor.fetchall(), type(cursor.fetchall()), sep="\n")
-
-
-connection_object_to_testdb.commit
-
-connection_object_to_testdb.close()"""
 
 
 def cast_vote(connection_to_votesdb, party_name):
