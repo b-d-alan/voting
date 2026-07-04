a218c28f69d78938c7d1a3295f3b7f0fb319130a
b-d-alan <balthasardanielo@gmail.com>
2026-07-04 19:46:33 +0530

    Add initial GUI implementation for election voting application

---
+++ gui.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+import index
+
+app = ctk.CTk()
+app.title("ELECTION")
+app.geometry("1600x600")
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+button = ctk.CTkButton(master=app, text="Vote", command=index.cast_vote)
+
+button.pack()
+app.mainloop()
